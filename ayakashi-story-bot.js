949a035a97f825e1831f87dbd78fcb3f84f44174
Ke Sun <kesun15@gmail.com>
2014-06-20 13:37:05 -0400

    still need to keep script persistent

--- ayakashi-story-bot.js
+++ ayakashi-story-bot.js
@@ -3,14 +3,35 @@ var max = 30;
 var continueClick = true;
 function click() {
     var delay = 1000;
-    console.log(current + "/" + max);
         if ($("#card-acquisition-page").hasClass("ui-page-active")) {
             window.history.back();
+        } else if ($("#parts-complete-page").hasClass("ui-page-active")) {
+            window.history.back();
+        } else if ($("#npc-battle-confirm-page").hasClass("ui-page-active")) {
+            delay = 2000;
+            $link = $('a:first');
+            $link[0].click();
+        } else if ($(".results").hasClass("ui-page-active")) {
+            $link = $('a:first');
+            $link[0].click();
+        } else if ($("#negotiation-failed-page").hasClass("ui-page-active")) {
+            delay = 2000;
+            location.href = "http://zc2.ayakashi.zynga.com/app.php?_c=adventure&action=currentStage";
+        } else if ($("#negotiation-page").hasClass("ui-page-active")) {
+            $link = $('a:first');
+            $link[0].click();
+        } else if ($("#drama-page").hasClass("ui-page-active")) {
+            var str = window.location.search;
+            str = str.substr(str.indexOf("http"));
+            str = str.substr(0, str.indexOf("&"));
+            location.href = unescape(str);
+            delay = 2000;
+        } else if ($("#battle-page").hasClass("ui-page-active")) {
+            $("#node-7").trigger("click");
         } else if ($("#treasure-acquisition-page").hasClass("ui-page-active")) {
             window.history.back();
         } else if ($("#level-up-page").hasClass("ui-page-active")) {
-            $(".button")[2].id = "continue";
-            $("#continue").trigger("click");
+            window.history.back();
         } else if ($("#encounter-other-player-page").hasClass("ui-page-active")) {
             window.history.back();
         } else if ($("#parts-pvp-acquisition-page").hasClass("ui-page-active")) {
