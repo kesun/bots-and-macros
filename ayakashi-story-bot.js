278f8b7b0b46746fe40e5761071f5c57aaccc8d2
Ke Sun <kesun15@gmail.com>
2014-07-25 11:09:00 -0400

    Still a few issues

--- ayakashi-story-bot.js
+++ ayakashi-story-bot.js
@@ -2,7 +2,7 @@ var current = 0;
 var max = 30;
 var continueClick = true;
 var curURL = document.URL;
-var newWindow = window.open(curURL, "_blank", "width=300, height=500");
+var newWindow = window.open(curURL, "_blank", "width=350, height=500");
 var delay = 1000;
 
 function init() {
@@ -18,7 +18,7 @@ function init() {
         } else if (newWindow.$("#npc-battle-confirm-page").hasClass("ui-page-active")) {
             $link = newWindow.$('a:first');
             $link[0].click();
-        } else if (newWindow.$(".results").hasClass("ui-page-active")) {
+        } else if (newWindow.$("#won").hasClass("ui-page-active")) { //ISSUE
             $link = newWindow.$('a:first');
             $link[0].click();
         } else if (newWindow.$("#negotiation-failed-page").hasClass("ui-page-active")) {
@@ -26,11 +26,12 @@ function init() {
         } else if (newWindow.$("#negotiation-page").hasClass("ui-page-active")) {
             $link = newWindow.$('a:first');
             $link[0].click();
-        } else if (newWindow.$("#drama-page").hasClass("ui-page-active")) {
+        } else if (newWindow.$("#drama-page").hasClass("ui-page-active")) { //ISSUE
             var str = newWindow.location.search;
             str = str.substr(str.indexOf("http"));
             str = str.substr(0, str.indexOf("&"));
-            newWindow.location.href = unescape(str);
+            str = unescape(str);
+            newWindow.location.href = str;
         } else if (newWindow.$("#battle-page").hasClass("ui-page-active")) {
             newWindow.$("#node-7").trigger("click");
         } else if (newWindow.$("#treasure-acquisition-page").hasClass("ui-page-active")) {
