5512d4693a47224841e380848fbaebd38f45c76b
Ke Sun <kesun15@gmail.com>
2014-08-08 13:19:06 -0400

    Update ayakashi-story-bot.js

--- ayakashi-story-bot.js
+++ ayakashi-story-bot.js
@@ -2,11 +2,8 @@ var current = 0;
 var max = 30;
 var continueClick = true;
 var curURL = document.URL;
-<<<<<<< HEAD
 var newWindow = window.open(curURL, "_blank", "width=400, height=500");
-=======
 var newWindow = window.open(curURL, "_blank", "width=350, height=500");
->>>>>>> 278f8b7b0b46746fe40e5761071f5c57aaccc8d2
 var delay = 1000;
 
 function init() {
@@ -22,7 +19,6 @@ function init() {
         } else if (newWindow.$("#npc-battle-confirm-page").hasClass("ui-page-active")) {
             $link = newWindow.$('a:first');
             $link[0].click();
-<<<<<<< HEAD
         } else if (newWindow.$("#won").hasClass("ui-page-active")) {
             $link = newWindow.$('a:first');
             //$link[0].click();
@@ -33,7 +29,6 @@ function init() {
             $link[0].click();
         } else if (newWindow.$("#drama-page").hasClass("ui-page-active")) {
             newWindow.location.href = "http://zc2.ayakashi.zynga.com/app.php?hid=last&encounter_battle_mode=1&_c=npc_battle&action=battle_scene";
-=======
         } else if (newWindow.$("#won").hasClass("ui-page-active")) { //ISSUE
             $link = newWindow.$('a:first');
             $link[0].click();
@@ -48,7 +43,6 @@ function init() {
             str = str.substr(0, str.indexOf("&"));
             str = unescape(str);
             newWindow.location.href = str;
->>>>>>> 278f8b7b0b46746fe40e5761071f5c57aaccc8d2
         } else if (newWindow.$("#battle-page").hasClass("ui-page-active")) {
             newWindow.$("#node-7").trigger("click");
         } else if (newWindow.$("#treasure-acquisition-page").hasClass("ui-page-active")) {
@@ -61,10 +55,7 @@ function init() {
             newWindow.history.back();
         } else {
             if (!(newWindow.$("#do-adventure").hasClass("loading"))) {
-<<<<<<< HEAD
-=======
                 console.log('if loading');
->>>>>>> 278f8b7b0b46746fe40e5761071f5c57aaccc8d2
                 if (current < max) {
                     newWindow.$("#do-adventure").trigger("click");
                 } else {
@@ -74,7 +65,6 @@ function init() {
             }
         }
     }
-<<<<<<< HEAD
     setTimeout(init, delay);
 }
 
@@ -83,7 +73,6 @@ function start(){
 }
 
 start();
-=======
 
     setTimeout(init, delay);
 }
@@ -93,4 +82,3 @@ function start(){
 }
 
 start();
->>>>>>> 278f8b7b0b46746fe40e5761071f5c57aaccc8d2
