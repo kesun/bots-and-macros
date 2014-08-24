ea45bb8da684c6798635ed5117000bdfd5376d9e
Ke Sun <kesun15@gmail.com>
2014-08-24 12:23:05 -0400

    can now auto-complete sealstone sets till AS is out

--- lowbie-battle-crawler.js
+++ lowbie-battle-crawler.js
@@ -1,111 +1,90 @@
 var newWindow;
-var wins = 0;
 var maxDS = 30; // EDIT THIS ACCORDING TO NEEDS
-var desiredWins = 2; // EDIT THIS ACCORDING TO NEEDS
+var eventID = 63 // EDIT THIS ACCORDING TO NEEDS
+var curItem;
 
-function crawl(playerList){
-	if(document.readyState == 'complete' && !$(document.getElementsByTagName('html')).hasClass('ui-loading')){
-		var i = 0;
-		for(i = 0; i < playerList.length; i++){
-			var def = $($(playerList[i]).find('dl').children()[3]).text();
-			if(def <= maxDS){
-				openBattle(i);
-				break;
-			}
-		}
-		if(i == playerList.length){
-			setTimeout(init, 100);
-		}
-	}else{
-		setTimeout(function(){crawl(playerList)}, 1000);
-	}
-}
+function openBattle(item, player){
 
-function openBattle(index){
-	console.log('openBattle');
-	var addr = "http://zc2.ayakashi.zynga.com" + $($('#opponents-list').find('li')[index]).find('a').attr('href');
-	newWindow = window.open(addr, "_blank", "width=400, height=500");
-	fight(0);
-}
-
-function fight(state){
-	console.log('fight');
-	if(state == 0){
-		if(newWindow.document.readyState == 'complete' && !$(newWindow.document.getElementsByTagName('html')).hasClass('ui-loading')){
-			if(newWindow.document.getElementsByTagName('body')[0].childElementCount > 0){
-				if(newWindow.$("#battle-confirm").hasClass('ui-page-active')){
-					var addr = newWindow.$('#battle-button-panel').find('a').attr('href');
-					console.log('FIGHT: ', addr);
-					var battleWindow = window.open(addr, "_blank", "width=500, height=500");
-					checkFight(battleWindow, 0);
-					setTimeout(function(){fight(1)}, 500);
-				}else{
-					newWindow.close();
-					newWindow = undefined;
-					setTimeout(init, 300);
-				}
-			}else{
-				setTimeout(function(){fight(0)}, 1000);
-			}
-		}else{
-			setTimeout(function(){fight(0)}, 1000);
-		}
-	}else{ // done
-		newWindow.close();
-		newWindow = undefined;
-	}
+    console.log('openBattle');
+    var addr = "http://zc2.ayakashi.zynga.com/app.php?_c=parts_pvp_event&action=exec_battle&target_user_id=" + player + "&target_item_id=" + item + "&evid=" + eventID;
+    var battleWindow = window.open(addr, "_blank", "width=400, height=500");
+    checkFight(battleWindow, 0);
 }
 
 function checkFight(battleWindow, state){
-	if(state == 0){
-		console.log(battleWindow.document);
-		battleWindow.document;
-		if(battleWindow.document.readyState == 'complete' && battleWindow.document.getElementsByTagName('body')[0].innerHTML != ""){
-			if(battleWindow.$('#empty-energy-page').length > 0){
-				// deal with empty energy
-				console.log('empty energy');
-			}else if(battleWindow.$("#battle-page").length > 0){
-				// BATTLEZ!
-				console.log('WIN!');
-				wins++;
-				setTimeout(function(){checkFight(battleWindow, 1)}, 100);
-			}else if(battleWindow.$("#parts-pvp-battle-no-item").length > 0){
-				// stone is already gone
-				console.log('stone is gone...');
-				setTimeout(function(){checkFight(battleWindow, 1)}, 100);
-			}
-		}else{
-			setTimeout(function(){checkFight(battleWindow, 0)}, 500);
-		}
-	}else{ // done
-		battleWindow.close();
-		battleWindow = undefined;
-		setTimeout(init, 300);
-	}
-	
-}
-
-function getPlayerList(){
-	console.log('getPlayerList');
-	if(!$(document.getElementsByTagName('html')).hasClass('ui-loading')){
-		crawl($('#opponents-list').find('li'));
-	}else{
-		setTimeout(getPlayerList, 500);
-	}
-}
-
-function init() {
-	if(wins < desiredWins){
-	    if(document.readyState == 'complete' && !$(document.getElementsByTagName('html')).hasClass('ui-loading')){
-	        $('#update-battle-list').click();
-	        setTimeout(getPlayerList, 500);
-	    }else{
-	    	setTimeout(init, 1000);
-	    }
-	}else{
-		console.log("SUMMARY: " + wins + " stones in the bag.");
-	}
+    if(state == 0){
+        if(battleWindow.document.readyState == 'complete' && battleWindow.document.getElementsByTagName('body')[0].innerHTML != ""){
+            if(battleWindow.$('#empty-energy-page').length > 0){
+                // deal with empty energy
+                console.log('empty energy');
+            }else if(battleWindow.$("#battle-page").length > 0){
+                // BATTLEZ!
+                console.log('WIN!');
+                setTimeout(function(){checkFight(battleWindow, 1)}, 100);
+            }else if(battleWindow.$("#parts-pvp-battle-no-item").length > 0){
+                // stone is already gone
+                console.log('stone is gone...');
+                setTimeout(function(){checkFight(battleWindow, 1)}, 100);
+            }
+        }else{
+            setTimeout(function(){checkFight(battleWindow, 0)}, 500);
+        }
+    }else{ // done
+        battleWindow.close();
+        battleWindow = undefined;
+        setTimeout(init, 300);
+    }
     
 }
 
+function crawl(){
+    console.log('');
+    if(newWindow.document.readyState == 'complete' && !$(newWindow.document.getElementsByTagName('html')).hasClass('ui-loading') && newWindow.$('.defense-kiai').length > 0){
+        var playerList = newWindow.$('#opponents-list').find('li');
+        var i = 0;
+        for(i = 0; i < playerList.length; i++){
+            var player = $(playerList[i]);
+            var def = $(player.find('dl').children()[3]).text();
+            if(def <= maxDS){
+                var zid = player.find('a').attr('href').split('&')[2].split('=')[1]
+                openBattle(curItem, zid);
+                break;
+            }
+        }
+        if(i == playerList.length){
+            newWindow.$('#update-battle-list').click();
+            setTimeout(crawl, 500);
+        }
+    }else{
+        setTimeout(crawl, 500);
+    }
+}
+
+function openNewWindow(){
+    console.log('openNewWindow');
+    if(newWindow.document.readyState == 'complete' && newWindow.document.getElementsByTagName('body')[0].innerHTML != "" && newWindow.$('.parts').length > 0){
+        var stoneList = newWindow.$('.parts');
+        var index = 0;
+        while(stoneList[index].getElementsByTagName('span')[0].getAttribute('style') != null){
+            index++;
+        }
+        curItem = index + 1;
+        var stoneURL = "http://zc2.ayakashi.zynga.com/app.php?_c=parts_pvp_event&action=battle_list&evid=" + eventID + "&target_item_id=" + curItem;
+        newWindow.open(stoneURL, "_self");
+        console.log('going to getPlayerList');
+        crawl();
+    }else{
+        setTimeout(openNewWindow, 1000);
+    }
+}
+
+function init() {
+    console.log('init');
+    var summaryURL = "http://zc2.ayakashi.zynga.com/app.php?_c=parts_pvp_event&action=parts_list&evid=" + eventID;
+    newWindow.open(summaryURL, "_self");
+    console.log('going to openNewWindow');
+    openNewWindow();
+}
+
+newWindow = window.open("", "_blank", "width=400, height=500");
 init();
