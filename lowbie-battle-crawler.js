bf23d235c0ac820b02e01a798ce9ec93795405c0
Ke Sun <kesun15@gmail.com>
2014-08-27 14:37:25 -0400

    Adding crawling for stock maga, UNFINISHED, wip.

--- lowbie-battle-crawler.js
+++ lowbie-battle-crawler.js
@@ -1,13 +1,20 @@
 var newWindow;
 var maxDS = 30; // EDIT THIS ACCORDING TO NEEDS
 var eventID = 63 // EDIT THIS ACCORDING TO NEEDS
+var staticMaga = "http://zc2.ayakashi.zynga.com/app.php?_c=parts&action=list";
 var curItem;
+var targetType = 0; // 0 for stock bunny maga, 1 for event maga. EDIT THIS ACCORDING TO NEEDS
 
 function openBattle(item, player){
-
     console.log('openBattle');
-    var addr = "http://zc2.ayakashi.zynga.com/app.php?_c=parts_pvp_event&action=exec_battle&target_user_id=" + player + "&target_item_id=" + item + "&evid=" + eventID;
-    var battleWindow = window.open(addr, "_blank", "width=400, height=500");
+    var addr;
+    var battleWindow;
+    if(targetType == 0){
+        addr = "http://zc2.ayakashi.zynga.com/app.php?_c=parts_pvp_event&action=exec_battle&target_user_id=" + player + "&target_item_id=" + item + "&evid=" + eventID;
+    }else{
+        addr = "http://zc2.ayakashi.zynga.com/app.php?_c=parts_pvp_event&action=exec_battle&target_user_id=" + player + "&target_item_id=" + item + "&evid=" + eventID;
+    }
+    battleWindow = window.open(addr, "_blank", "width=400, height=500");
     checkFight(battleWindow, 0);
 }
 
@@ -47,7 +54,7 @@ function crawl(){
             var def = $(player.find('dl').children()[3]).text();
             if(def <= maxDS){
                 var zid = player.find('a').attr('href').split('&')[2].split('=')[1]
-                openBattle(curItem, zid);
+                //openBattle(curItem, zid);
                 break;
             }
         }
@@ -60,16 +67,41 @@ function crawl(){
     }
 }
 
+function getMaga(){
+    if(newWindow.document.readyState == 'complete' && newWindow.document.getElementsByTagName('body')[0].innerHTML != "" && newWindow.$('.parts').length > 0){
+
+    }else{
+        setTimeout(getMaga, 1000);
+    }
+}
+
 function openNewWindow(){
     console.log('openNewWindow');
     if(newWindow.document.readyState == 'complete' && newWindow.document.getElementsByTagName('body')[0].innerHTML != "" && newWindow.$('.parts').length > 0){
-        var stoneList = newWindow.$('.parts');
+        var stoneList;
+        if(targetType == 0){
+            var claim = newWindow.$('.parts-list')[3].getElementsByTagName('button')[0];
+            if(!$(claim).hasClass('disabled')){
+                $(claim).click();
+                getMaga();
+            }
+            stoneList = newWindow.$('.parts-selector')[3].getElementsByClassName('parts');
+        }else{
+            stoneList = newWindow.$('.parts');
+        }
         var index = 0;
-        while(stoneList[index].getElementsByTagName('span')[0].getAttribute('style') != null){
+        while(stoneList[index].getElementsByTagName('span')[0].getAttribute('style') != ""){
+            console.log(stoneList[index].getElementsByTagName('span')[0].getAttribute('style'));
             index++;
         }
         curItem = index + 1;
-        var stoneURL = "http://zc2.ayakashi.zynga.com/app.php?_c=parts_pvp_event&action=battle_list&evid=" + eventID + "&target_item_id=" + curItem;
+        var stoneURL;
+        if(targetType == 0){
+            stoneURL = "http://zc2.ayakashi.zynga.com/app.php?target_parts_id=68" + curItem + "&from_where=stone&_c=battle&action=battle_list&tutorial_step=41";
+        }else{
+            stoneURL = "http://zc2.ayakashi.zynga.com/app.php?_c=parts_pvp_event&action=battle_list&evid=" + eventID + "&target_item_id=" + curItem;
+
+        }
         newWindow.open(stoneURL, "_self");
         console.log('going to getPlayerList');
         crawl();
@@ -80,8 +112,12 @@ function openNewWindow(){
 
 function init() {
     console.log('init');
-    var summaryURL = "http://zc2.ayakashi.zynga.com/app.php?_c=parts_pvp_event&action=parts_list&evid=" + eventID;
-    newWindow.open(summaryURL, "_self");
+    if(targetType == 0){
+        newWindow.open(staticMaga, "_self");
+    }else{
+        var summaryURL = "http://zc2.ayakashi.zynga.com/app.php?_c=parts_pvp_event&action=parts_list&evid=" + eventID;
+        newWindow.open(summaryURL, "_self");
+    }
     console.log('going to openNewWindow');
     openNewWindow();
 }
