0f424e9521ea2b57c69aae5bd9f711fcce251000
Ke Sun <kesun15@gmail.com>
2014-10-06 10:15:30 -0400

    Update ayakashi-tower-climb.js

--- ayakashi-tower-climb.js
+++ ayakashi-tower-climb.js
@@ -2,18 +2,25 @@
 var eventID = 67;
 
 var teamInfo = {
-	101431: [10472, 10415, 10495, 10416, 10493],
-	101432: [14198, 10492, 3332, 3839, 10046],
-	101433: [16453, 17579, 17198, 10046, 10495],
-	101434: [10181, 10472, 11290, 17225, 17306],
-	1014232: [17306, 10415, 10495, 10416, 10493]
+    101431: [10472, 10415, 10495, 10416, 10493],
+    101432: [14198, 10492, 3332, 3839, 10046],
+    101433: [16453, 17579, 17198, 10046, 10495],
+    101434: [10181, 10472, 11290, 17225, 17306],
+    101435: [],
+    111431: [],
+    111433: [],
+    111434: [],
+    111435: [],
+    111436: [],
+    111437: [],
+    1014232: [17306, 10415, 10495, 10416, 10493]
 };
 // 613064 = dora
 var battleInfo = {
-	101431: 613050,
-	101432: 613052, // 53 = win drama
-	101433: 613056,
-	101434: 613054
+    101431: 613050,
+    101432: 613052, // 53 = win drama
+    101433: 613056,
+    101434: 613054
 };
 
 var staticAdventure = "http://zc2.ayakashi.zynga.com/app.php?_c=extra_quest_event_adventure&evid=";
@@ -31,180 +38,186 @@ var dramaID;
 var adventureURL = staticAdventure + eventID;
 
 function negotiation() {
-	console.log('negotiation');
-	if(newWindow.document.readyState == 'complete' &&
-		newWindow.document.getElementsByTagName('body')[0].innerHTML != "" &&
-		!$(newWindow.document.getElementsByTagName('html')).hasClass('ui-loading') &&
-		newWindow.$('#negotiation-page').length > 0) {
-		console.log(battleID);
-		switch(battleID) {
-			case 101431:
-			case 101432:
-			case 101433:
-			case 101434:
-				newWindow.open($(newWindow.$('.button')[1]).attr('href'), '_self'); /////////////////////////////////////////////////////
-				break;
-		}
-		setTimeout(climb, 2000);
-	}else{
-		setTimeout(negotiation, 1000);
-	}
+    console.log('negotiation');
+    if(newWindow.document.readyState == 'complete' &&
+        newWindow.document.getElementsByTagName('body')[0].innerHTML != "" &&
+        !$(newWindow.document.getElementsByTagName('html')).hasClass('ui-loading') &&
+        newWindow.$('#negotiation-page').length > 0) {
+        console.log(battleID);
+        switch(battleID) {
+            case 101431:
+            case 101432:
+            case 101433:
+            case 101434:
+                newWindow.open($(newWindow.$('.button')[1]).attr('href'), '_self'); /////////////////////////////////////////////////////
+                break;
+        }
+        setTimeout(climb, 2000);
+    }else{
+        setTimeout(negotiation, 1000);
+    }
 }
 
 function checkBattle(state) { // state 0 = during battle, state 1 = after battle, state 2 = win, state 3 = lose
-	console.log('checkBattle');
-	if(newWindow.document.readyState == 'complete' &&
-		newWindow.document.getElementsByTagName('body')[0].innerHTML != "" &&
-		!$(newWindow.document.getElementsByTagName('html')).hasClass('ui-loading')) {
-		if(state == 0) {
-			console.log('state 0');
-			setTimeout(function(){
-				newWindow.$('.layer').trigger('click'); ////////////////////////////////////////////////////
-				setTimeout(function(){checkBattle(1);}, 1000);
-			}, 1000);
-			
-		}else if(state == 1) {
-			console.log('state 1');
-			if(newWindow.$('.results').hasClass('ui-page-active')){
-				if(newWindow.$('#extraquest-event-won-page').hasClass('ui-page-active')){
-					setTimeout(function(){checkBattle(2);}, 1000);
-				}else{
-					setTimeout(function(){checkBattle(3);}, 1000);
-				}
-			}else{
-				setTimeout(function(){checkBattle(1);}, 1000);
-			}
-		}else if(state == 2){
-			console.log('won!');
-			newWindow.open(staticNegotiation, '_self');
-			negotiation();
-		}else if(state == 3){
-			console.log('lost!');
-		}
-		
-	}else{
-		setTimeout(function(){checkBattle(state);}, 1000);
-	}
+    console.log('checkBattle');
+    if(newWindow.document.readyState == 'complete' &&
+        newWindow.document.getElementsByTagName('body')[0].innerHTML != "" &&
+        !$(newWindow.document.getElementsByTagName('html')).hasClass('ui-loading')) {
+        if(state == 0) {
+            console.log('state 0');
+            setTimeout(function(){
+                newWindow.$('.layer').trigger('click'); ////////////////////////////////////////////////////
+                setTimeout(function(){checkBattle(1);}, 1000);
+            }, 1000);
+            
+        }else if(state == 1) {
+            console.log('state 1');
+            if(newWindow.$('.results').hasClass('ui-page-active')){
+                if(newWindow.$('#extraquest-event-won-page').hasClass('ui-page-active')){
+                    setTimeout(function(){checkBattle(2);}, 1000);
+                }else{
+                    setTimeout(function(){checkBattle(3);}, 1000);
+                }
+            }else{
+                setTimeout(function(){checkBattle(1);}, 1000);
+            }
+        }else if(state == 2){
+            console.log('won!');
+            newWindow.open(staticNegotiation, '_self');
+            negotiation();
+        }else if(state == 3){
+            console.log('lost!');
+        }
+        
+    }else{
+        setTimeout(function(){checkBattle(state);}, 1000);
+    }
 }
 
 function checkDrama() {
-	console.log('checkDrama');
-	if(newWindow.document.readyState == 'complete' &&
-		newWindow.document.getElementsByTagName('body')[0].innerHTML != "" &&
-		!$(newWindow.document.getElementsByTagName('html')).hasClass('ui-loading')) {
+    console.log('checkDrama');
+    if(newWindow.document.readyState == 'complete' &&
+        newWindow.document.getElementsByTagName('body')[0].innerHTML != "" &&
+        !$(newWindow.document.getElementsByTagName('html')).hasClass('ui-loading')) {
 
-		console.log('battleInfo ID:', battleInfo[battleID]);
-		if(battleInfo[battleID] == $(newWindow.$('.button')[0]).attr('href').split('&')[0].split('=')[1]){
-			newWindow.open(staticBattle1 + battleID + staticBattle2, '_self');
-			checkBattle(0);
-		}
+        console.log('battleInfo ID:', battleInfo[battleID]);
+        if(battleInfo[battleID] == $(newWindow.$('.button')[0]).attr('href').split('&')[0].split('=')[1]){
+            newWindow.open(staticBattle1 + battleID + staticBattle2, '_self');
+            checkBattle(0);
+        }
 
-	}else{
-		setTimeout(checkDrama, 1000);
-	}
+    }else{
+        setTimeout(checkDrama, 1000);
+    }
 }
 
 function wipeTeam(teamWindow, state, members){ // state 0 = init loading; state 1 = wiping; state 2 = close
-	console.log('wipeTeam(state):', state);
-	if(teamWindow.document.readyState == 'complete' &&
-		teamWindow.document.getElementsByTagName('body')[0].innerHTML != "" &&
-		!$(teamWindow.document.getElementsByTagName('html')).hasClass('ui-loading')) {
+    console.log('wipeTeam(state):', state);
+    if(teamWindow.document.readyState == 'complete' &&
+        teamWindow.document.getElementsByTagName('body')[0].innerHTML != "" &&
+        !$(teamWindow.document.getElementsByTagName('html')).hasClass('ui-loading')) {
 
-		if(state == 0){
-			console.log('clicking offence:', $(teamWindow.$('.offense')[0]).parent());
-			$(teamWindow.$('.offense')[0]).click();
-			wipeTeam(teamWindow, 1, members);
-		}else if(state == 1){
-			if(teamWindow.$('.reset-monster').length > 0){
-				console.log('clicking reset:', $(teamWindow.$('.reset-monster')[0]));
-				$(teamWindow.$('.reset-monster')[1]).click();
-				wipeTeam(teamWindow, 2, members);
-			}else{
-				setTimeout(function(){
-					wipeTeam(teamWindow, state, members);
-				}, 1000);
-			}
-			
-		}else if(state == 2){
-			teamWindow.close();
-			updateTeam(members, 1, 0);
-		}
-	}else{
-		setTimeout(function(){
-			wipeTeam(teamWindow, state, members);
-		}, 1000);
-	}
+        if(state == 0){
+            console.log('clicking offence:', $(teamWindow.$('.offense')[0]).parent());
+            $(teamWindow.$('.offense')[0]).click();
+            wipeTeam(teamWindow, 1, members);
+        }else if(state == 1){
+            if(teamWindow.$('.reset-monster').length > 0){
+                console.log('clicking reset:', $(teamWindow.$('.reset-monster')[0]));
+                $(teamWindow.$('.reset-monster')[1]).click();
+                wipeTeam(teamWindow, 2, members);
+            }else{
+                setTimeout(function(){
+                    wipeTeam(teamWindow, state, members);
+                }, 1000);
+            }
+            
+        }else if(state == 2){
+            teamWindow.close();
+            updateTeam(members, 1, 0);
+        }
+    }else{
+        setTimeout(function(){
+            wipeTeam(teamWindow, state, members);
+        }, 1000);
+    }
 }
 
 function addMember(memberWindow, members, index){
-	console.log('addMember(index):', index);
-	if(memberWindow.document.readyState == 'complete' &&
-		memberWindow.document.getElementsByTagName('body')[0].innerHTML != "" &&
-		!$(memberWindow.document.getElementsByTagName('html')).hasClass('ui-loading')) {
+    console.log('addMember(index):', index);
+    if(memberWindow.document.readyState == 'complete' &&
+        memberWindow.document.getElementsByTagName('body')[0].innerHTML != "" &&
+        !$(memberWindow.document.getElementsByTagName('html')).hasClass('ui-loading')) {
 
-		switch(index) {
-			case 0:
-				memberWindow.$('.change-leader').click();
-				break;
-			case 1:
-			case 2:
-			case 3:
-			case 4:
-				memberWindow.$('.to-offense').click();
-				break;
-		}
-		setTimeout(function(){
-			memberWindow.close();
-			updateTeam(members, 1, index + 1);
-		}, 1000);
-	}else{
-		setTimeout(function(){
-			addMember(memberWindow, members, index);
-		}, 1000);
-	}
+        switch(index) {
+            case 0:
+                memberWindow.$('.change-leader').click();
+                break;
+            case 1:
+            case 2:
+            case 3:
+            case 4:
+                memberWindow.$('.to-offense').click();
+                break;
+        }
+        setTimeout(function(){
+            memberWindow.close();
+            updateTeam(members, 1, index + 1);
+        }, 1000);
+    }else{
+        setTimeout(function(){
+            addMember(memberWindow, members, index);
+        }, 1000);
+    }
 }
 
 function updateTeam(members, state, index){ // state 0 = wipe team, state 1 = add members, state 2 = done
-	console.log('updateTeam(state):', state);
-	if(state == 0){ // WIPE TEAM
-		var tempWindow = window.open(staticMonsterList, "_blank", "width=10, height=10");
-		wipeTeam(tempWindow, 0, members);
-	}else if(state == 1){ // ADD MEMBERS
-		if(index < 5){
-			var monsterURL = staticMonster + members[index];
-			var tempWindow = window.open(monsterURL, "_blank", "width=10, height=10");
-			addMember(tempWindow, members, index);
-		}else{
-			updateTeam(members, 2, 0);
-		}
-	}else if(state == 2){ // TO BATTLE
-		console.log('TO BATTLE!');
-		checkDrama();
-	}
+    console.log('updateTeam(state):', state);
+    if(members == ""){
+        setTimeout(init, 500);
+    }else{
+       if(state == 0){ // WIPE TEAM
+            var tempWindow = window.open(staticMonsterList, "_blank", "width=10, height=10");
+            wipeTeam(tempWindow, 0, members);
+        }else if(state == 1){ // ADD MEMBERS
+            if(index < 5){
+                var monsterURL = staticMonster + members[index];
+                var tempWindow = window.open(monsterURL, "_blank", "width=10, height=10");
+                addMember(tempWindow, members, index);
+            }else{
+                updateTeam(members, 2, 0);
+            }
+        }else if(state == 2){ // TO BATTLE
+            console.log('TO BATTLE!');
+            checkDrama();
+        } 
+    }
+    
 }
 
 function climb(){
-	console.log('climb');
-	if(newWindow.document.readyState == 'complete' &&
-		newWindow.document.getElementsByTagName('body')[0].innerHTML != "" &&
-		!$(newWindow.document.getElementsByTagName('html')).hasClass('ui-loading')) {
+    console.log('climb');
+    if(newWindow.document.readyState == 'complete' &&
+        newWindow.document.getElementsByTagName('body')[0].innerHTML != "" &&
+        !$(newWindow.document.getElementsByTagName('html')).hasClass('ui-loading')) {
 
-		if(newWindow.$('#card-acquisition-page').hasClass('ui-page-active') ||
-			newWindow.$('#event-point-acquisition-page').hasClass('ui-page-active')){
-			newWindow.history.back();
-			setTimeout(climb, 1000);
-		}else if(newWindow.$('#stage-page').hasClass('ui-page-active')){
-			floorNum = newWindow.$('#stage-name')[0].innerHTML.split("F")[0];
-			newWindow.$('#do-adventure').click();
-			setTimeout(climb, 1000);
-		}else if(newWindow.$('#npc-battle-confirm-page').hasClass('ui-page-active')){
-			battleID = newWindow.location.href.split('&')[0].split('=')[1];
-			console.log('ghost encountered: ', battleID);
-			updateTeam(teamInfo[battleID], 0, 0);
-		}
-	}else{
-		setTimeout(climb, 1000);
-	}
+        if(newWindow.$('#card-acquisition-page').hasClass('ui-page-active') ||
+            newWindow.$('#event-point-acquisition-page').hasClass('ui-page-active') ||
+            newWindow.$('#treasure-acquisition-page').hasClass('ui-page-active')){
+            newWindow.history.back();
+            setTimeout(climb, 1000);
+        }else if(newWindow.$('#stage-page').hasClass('ui-page-active')){
+            floorNum = newWindow.$('#stage-name')[0].innerHTML.split("F")[0];
+            newWindow.$('#do-adventure').click();
+            setTimeout(climb, 1000);
+        }else if(newWindow.$('#npc-battle-confirm-page').hasClass('ui-page-active')){
+            battleID = newWindow.location.href.split('&')[0].split('=')[1];
+            console.log('ghost encountered: ', battleID);
+            updateTeam(teamInfo[battleID], 0, 0);
+        }
+    }else{
+        setTimeout(climb, 1000);
+    }
 }
 
 function init() {
