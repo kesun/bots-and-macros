dec2dd0e1c6197073b1f1e891fad471c6f3b4995
Ke Sun <kesun15@gmail.com>
2014-10-07 14:56:38 -0400

    Update ayakashi-lowbie-sealstone-battle-crawler.js

--- ayakashi-lowbie-sealstone-battle-crawler.js
+++ ayakashi-lowbie-sealstone-battle-crawler.js
@@ -1,236 +1,174 @@
-// Tower Climbing
-var eventID = 67;
+// ---------------------- Editable Starts ----------------------
+// -------------------------------------------------------------
+var reloadTime = 14 // when the game is slow, increase this number as needed. Time in sec = reloadTime / 2.
+var maxDS = 30; // defence spirit cap of the target
+var targetType = 0; // 0 for stock maga, 1 for event maga
 
-var teamInfo = {
-    101431: [10472, 10415, 10495, 10416, 10493],
-    101432: [10492, 10472, 10181, 17306, 10495],
-    //101433: [16453, 17579, 17198, 10046, 10495],
-    //101434: [10181, 10472, 11290, 17225, 17306],
-    //101432: [],
-    101433: [],
-    101434: [],
-    101435: [],
-    111431: [],
-    111433: [],
-    111434: [],
-    111435: [],
-    111436: [],
-    111437: []
-};
-// 613064 = dora
-var battleInfo = {
-    101431: 613050,
-    101432: 613052, // 53 = win drama
-    101433: 613056,
-    101434: 613054
-};
+// For event (only matters if targetType == 1)
+var eventID = 67 // event ID
 
-var staticAdventure = "http://zc2.ayakashi.zynga.com/app.php?_c=extra_quest_event_adventure&evid=";
-var staticMonster = "http://zc2.ayakashi.zynga.com/app.php?_c=monster&action=details&inventory_monster_id=";
-var staticMonsterList = "http://zc2.ayakashi.zynga.com/app.php?_c=monster&action=list";
-var staticBattle1 = "http://zc2.ayakashi.zynga.com/app.php?target_user_id=";
-var staticBattle2 = "&evid=" + eventID + "&encounter_battle_mode=1&_c=extra_quest_event_npc_battle&action=exec_battle";
-var staticNegotiation = "http://zc2.ayakashi.zynga.com/app.php?encounter_battle_mode=1&evid=" + eventID + "&_c=extra_quest_event_negotiation&action=found";
+// For static maga (only matters if targetType == 0)
+var staticMagaID = 1438; // 66 = kaguya, 68 = hare
+var staticMagaTargetState = 0; // 0 if for auto complete maga set, 1 for repeating on the same stone
+var staticMagaTarget = 5; // 1~6, indicate the target stone colour (only matters if staticMagaTargetState == 1)
+// -------------------------------------------------------------
+// ----------------------- Editable Ends -----------------------
+
+var staticURLBase = "http://zc2.ayakashi.zynga.com/app.php?";
+var staticMaga = staticURLBase + "_c=parts&action=list";
+var staticNormalBattle = staticURLBase + "_c=battle&action=exec_battle";
+var staticEventBattle = staticURLBase + "_c=parts_pvp_event&action=exec_battle";
+var staticEventStone = staticURLBase + "_c=parts_pvp_event&action=battle_list";
+var staticEventEntry = staticURLBase + "_c=parts_pvp_event&action=entry";
+//var staticGhostIndex = staticMagaID - 64 - 1; // this formula only works for the first half of the ghosts
+var staticGhostIndex = 0;
 
 var newWindow;
-var floorNum = 0;
-var battleID;
-var dramaID;
+var curItem;
 
-var adventureURL = staticAdventure + eventID;
-
-function negotiation() {
-    console.log('negotiation');
-    if(newWindow.document.readyState == 'complete' &&
-        newWindow.document.getElementsByTagName('body')[0].innerHTML != "" &&
-        !$(newWindow.document.getElementsByTagName('html')).hasClass('ui-loading') &&
-        newWindow.$('#negotiation-page').length > 0) {
-        console.log(parseInt(battleID));
-        switch(parseInt(battleID)) {
-            case 101431:
-            case 101432:
-            case 101433:
-            case 101434:
-                var cancelURL = "http://zc2.ayakashi.zynga.com/app.php?_c=extra_quest_event_negotiation&action=resign&evid=" + eventID;
-                newWindow.open(cancelURL, '_self'); /////////////////////////////////////////////////////
-                break;
+function openBattle(item, player){
+    var addr;
+    var battleWindow;
+    if(targetType == 0){
+        if(staticMagaTargetState == 0){
+            addr = staticNormalBattle + "&target_user_id=" + player + "&target_parts_id=" + staticMagaID + item + "&from_battle_tab=&ref=undefined";
+        }else{
+            addr = staticNormalBattle + "&target_user_id=" + player + "&target_parts_id=" + staticMagaID + staticMagaTarget + "&from_battle_tab=&ref=undefined";
         }
-        setTimeout(init, 2000);
     }else{
-        setTimeout(negotiation, 1000);
+        addr = staticEventBattle + "&target_user_id=" + player + "&target_item_id=" + item + "&evid=" + eventID;
     }
+    battleWindow = window.open(addr, "_blank", "width=10, height=10");
+    checkFight(battleWindow, 0);
 }
 
-function checkBattle(state) { // state 0 = during battle, state 1 = after battle, state 2 = win, state 3 = lose
-    console.log('checkBattle', state);
-    if(newWindow.document.readyState == 'complete' &&
-        newWindow.document.getElementsByTagName('body')[0].innerHTML != "" &&
-        !$(newWindow.document.getElementsByTagName('html')).hasClass('ui-loading')) {
-        if(state == 0) {
-            if(newWindow.$('#battle-page').hasClass('ui-page-active')){
-                setTimeout(function(){
-                    newWindow.$('.layer').trigger('click'); ////////////////////////////////////////////////////
-                    setTimeout(function(){checkBattle(1);}, 1000);
-                }, 1000); 
-            }else{
-                setTimeout(function(){checkBattle(0);}, 1000);
+function checkFight(battleWindow, state){
+    if(state == 0){
+        if(battleWindow.document.readyState == 'complete' && battleWindow.document.getElementsByTagName('body')[0].innerHTML != ""){
+            if(battleWindow.$('.alert').length > 0){
+                // alert page occurs
+                setTimeout(function(){checkFight(battleWindow, 1)}, 500);
+            }else if(battleWindow.$('#empty-energy-page').length > 0){
+                // deal with empty energy
+                var drink = battleWindow.$('.guts-recovery').attr("href");
+                battleWindow.open(drink, "_self");
+                setTimeout(function(){checkFight(battleWindow, 1)}, 1000);
+                console.log('empty energy');
+            }else if(battleWindow.$("#battle-page").length > 0){
+                // BATTLEZ!
+                console.log('WIN!');
+                setTimeout(function(){checkFight(battleWindow, 1)}, 100);
+            }else if(battleWindow.$("#parts-pvp-battle-no-item").length > 0){
+                // stone is already gone
+                console.log('stone is gone...');
+                setTimeout(function(){checkFight(battleWindow, 1)}, 100);
             }
-        }else if(state == 1) {
-            if(newWindow.$('.results').hasClass('ui-page-active')){
-                if(newWindow.$('#extraquest-event-won-page').hasClass('ui-page-active')){
-                    setTimeout(function(){checkBattle(2);}, 1000);
-                }else{
-                    setTimeout(function(){checkBattle(3);}, 1000);
-                }
-            }else{
-                setTimeout(function(){checkBattle(1);}, 1000);
-            }
-        }else if(state == 2){
-            console.log('won!');
-            newWindow.open(staticNegotiation, '_self');
-            negotiation();
-        }else if(state == 3){
-            console.log('lost!');
-            setTimeout(init, 500);
+        }else{
+            setTimeout(function(){checkFight(battleWindow, 0)}, 500);
         }
-        
-    }else{
-        setTimeout(function(){checkBattle(state);}, 1000);
-    }
-}
-
-function checkDrama() {
-    console.log('checkDrama');
-    if(newWindow.document.readyState == 'complete' &&
-        newWindow.document.getElementsByTagName('body')[0].innerHTML != "" &&
-        !$(newWindow.document.getElementsByTagName('html')).hasClass('ui-loading')) {
-
-        if(battleInfo[battleID] == $(newWindow.$('.button')[0]).attr('href').split('&')[0].split('=')[1]){
-            newWindow.open(staticBattle1 + battleID + staticBattle2, '_self');
-            checkBattle(0);
-        }
-
-    }else{
-        setTimeout(checkDrama, 1000);
-    }
-}
-
-function wipeTeam(teamWindow, state, members){ // state 0 = init loading; state 1 = wiping; state 2 = close
-    if(teamWindow.document.readyState == 'complete' &&
-        teamWindow.document.getElementsByTagName('body')[0].innerHTML != "" &&
-        !$(teamWindow.document.getElementsByTagName('html')).hasClass('ui-loading')) {
-
-        if(state == 0){
-            $(teamWindow.$('.offense')[0]).click();
-            wipeTeam(teamWindow, 1, members);
-        }else if(state == 1){
-            if(teamWindow.$('.reset-monster').length > 0){
-                $(teamWindow.$('.reset-monster')[1]).click();
-                wipeTeam(teamWindow, 2, members);
-            }else{
-                setTimeout(function(){
-                    wipeTeam(teamWindow, state, members);
-                }, 1000);
-            }
-            
-        }else if(state == 2){
-            teamWindow.close();
-            updateTeam(members, 1, 0);
-        }
-    }else{
-        setTimeout(function(){
-            wipeTeam(teamWindow, state, members);
-        }, 1000);
-    }
-}
-
-function addMember(memberWindow, members, index){
-    if(memberWindow.document.readyState == 'complete' &&
-        memberWindow.document.getElementsByTagName('body')[0].innerHTML != "" &&
-        !$(memberWindow.document.getElementsByTagName('html')).hasClass('ui-loading')) {
-
-        switch(index) {
-            case 0:
-                memberWindow.$('.change-leader').click();
-                break;
-            case 1:
-            case 2:
-            case 3:
-            case 4:
-                memberWindow.$('.to-offense').click();
-                break;
-        }
-        setTimeout(function(){
-            memberWindow.close();
-            updateTeam(members, 1, index + 1);
-        }, 1000);
-    }else{
-        setTimeout(function(){
-            addMember(memberWindow, members, index);
-        }, 1000);
-    }
-}
-
-function updateTeam(members, state, index){ // state 0 = wipe team, state 1 = add members, state 2 = done
-    if(members == ""){
-        setTimeout(init, 500);
-    }else{
-       if(state == 0){ // WIPE TEAM
-            var tempWindow = window.open(staticMonsterList, "_blank", "width=10, height=10");
-            wipeTeam(tempWindow, 0, members);
-        }else if(state == 1){ // ADD MEMBERS
-            if(index < 5){
-                var monsterURL = staticMonster + members[index];
-                var tempWindow = window.open(monsterURL, "_blank", "width=10, height=10");
-                addMember(tempWindow, members, index);
-            }else{
-                updateTeam(members, 2, 0);
-            }
-        }else if(state == 2){ // TO BATTLE
-            console.log('TO BATTLE!');
-            checkDrama();
-        } 
+    }else{ // done
+        battleWindow.close();
+        battleWindow = undefined;
+        setTimeout(init, 300);
     }
     
 }
 
-function climb(){
-    console.log('climb');
-    if(newWindow.document.readyState == 'complete' &&
-        newWindow.document.getElementsByTagName('body')[0].innerHTML != "" &&
-        !$(newWindow.document.getElementsByTagName('html')).hasClass('ui-loading')) {
+function crawl(counter){
+    if(newWindow.document.readyState == 'complete' && !$(newWindow.document.getElementsByTagName('html')).hasClass('ui-loading') && newWindow.$('.defense-kiai').length > 0){
+        var urlSplit = newWindow.location.href.split("&");
+        if(urlSplit.length >= 2 && (urlSplit[1] == "action=battle_list" || urlSplit[3] == "action=battle_list")){
+            var playerList = newWindow.$('#opponents-list').find('li');
+            var i = 0;
+            for(i = 0; i < playerList.length; i++){
+                var player = $(playerList[i]);
+                var def = $(player.find('dl').children()[3]).text();
+                if(def <= maxDS){
+                    var zid = player.find('a').attr('href').split('&')[2].split('=')[1]
+                    openBattle(curItem, zid);
+                    break;
+                }
+            }
+            if(i == playerList.length){
+                newWindow.$('#update-battle-list').click();
+                setTimeout(function(){crawl(0)}, 500);
+            }
+        }else{
+            setTimeout(init, 1000);
+        }
+        
+    }else{
+        var count = 0;
+        if(counter == reloadTime){
+            newWindow.location.reload(true);
+        }else{
+            count = counter + 1;
+        }
+        setTimeout(function(){crawl(count)}, 500);
+    }
+}
 
-        if(newWindow.$('#card-acquisition-page').hasClass('ui-page-active') ||
-            newWindow.$('#event-point-acquisition-page').hasClass('ui-page-active') ||
-            newWindow.$('#treasure-acquisition-page').hasClass('ui-page-active')){
-            newWindow.history.back();
-            setTimeout(climb, 1000);
-        }else if(newWindow.$('#stage-page').hasClass('ui-page-active')){
-            floorNum = newWindow.$('#stage-name')[0].innerHTML.split("F")[0];
-            newWindow.$('#do-adventure').click();
-            setTimeout(climb, 1000);
-        }else if(newWindow.$('#npc-battle-confirm-page').hasClass('ui-page-active')){
-            battleID = newWindow.location.href.split('&')[0].split('=')[1];
-            console.log('ghost encountered: ', battleID);
-            updateTeam(teamInfo[battleID], 0, 0);
-        }else if(newWindow.$('#negotiation-page').length > 0){
-            var cancelURL = "http://zc2.ayakashi.zynga.com/app.php?_c=extra_quest_event_negotiation&action=resign&evid=" + eventID;
-            newWindow.open(cancelURL, '_self');
-            setTimeout(climb, 2000);
+function getMaga(){
+    if(newWindow.document.readyState == 'complete' && $('#parts-reward-accept-page').hasClass('ui-page-active')){
+        console.log('got a maga!');
+        setTimeout(init, 1000);
+    }else{
+        setTimeout(getMaga, 1000);
+    }
+}
+
+function openNewWindow(state, stoneList){
+    if(newWindow.document.readyState == 'complete' && newWindow.document.getElementsByTagName('body')[0].innerHTML != "" && newWindow.$('.parts').length > 0){
+        if (state == 0){
+            if(targetType == 0){
+                console.log("staticGhostIndex", staticGhostIndex);
+                var claim = newWindow.$('.parts-list')[staticGhostIndex].getElementsByTagName('input')[3];
+                if(newWindow.$('.accept-reward').length > 0){
+                    console.log(newWindow.$('.accept-reward'));
+                    $(newWindow.$('.accept-reward')[0].getElementsByClassName('button')).trigger('click');
+                    getMaga();
+                }else{
+                    openNewWindow(1, newWindow.$('.parts-selector')[staticGhostIndex].getElementsByClassName('parts'));   
+                }
+            }else{
+                openNewWindow(1, newWindow.$('.parts'));
+            }
+        }else{
+            var index = 0;
+            //console.log(stoneList[index].getElementsByTagName('span')[0].getAttribute('style') == null);
+            while(stoneList[index].getElementsByTagName('span')[0].getAttribute('style') != ""){
+                index++;
+            }
+            console.log('index', index);
+            curItem = index + 1;
+            var stoneURL;
+            if(targetType == 0){
+                if(staticMagaTargetState == 0)
+                    stoneURL = staticURLBase + "target_parts_id=" + staticMagaID + curItem + "&from_where=stone&_c=battle&action=battle_list&tutorial_step=41";
+                else{
+                    stoneURL = staticURLBase + "target_parts_id=" + staticMagaID + staticMagaTarget + "&from_where=stone&_c=battle&action=battle_list&tutorial_step=41"
+                }
+            }else{
+                stoneURL = staticEventStone + "&evid=" + eventID + "&target_item_id=" + curItem;
+
+            }
+            newWindow.open(stoneURL, "_self");
+            crawl(0);
         }
     }else{
-        setTimeout(climb, 1000);
+        setTimeout(function(){openNewWindow(state, stoneList)}, 1000);
     }
 }
 
 function init() {
-    if(newWindow.document.readyState == 'complete' &&
-        !$(newWindow.document.getElementsByTagName('html')).hasClass('ui-loading')) {
-        newWindow.open(adventureURL, "_self");
-        setTimeout(climb, 1000);
+    if(targetType == 0){
+        newWindow.open(staticMaga, "_self");
     }else{
-        setTimeout(init, 1000);
+        // var summaryURL = "http://zc2.ayakashi.zynga.com/app.php?_c=parts_pvp_event&action=parts_list&evid=" + eventID;
+        var summaryURL = staticEventEntry + "&evid=66";
+        newWindow.open(summaryURL, "_self");
     }
+    openNewWindow(0, null);
 }
 
 newWindow = window.open("", "_blank", "width=10, height=10");
