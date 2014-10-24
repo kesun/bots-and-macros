d354c81ed9b17e34c745a30d556d1cf1c83cdea9
Ke Sun <kesun15@gmail.com>
2014-10-24 11:18:59 -0400

    Update ayakashi-story-bot.js

--- ayakashi-story-bot.js
+++ ayakashi-story-bot.js
@@ -3,36 +3,33 @@ var max = 30;
 var continueClick = true;
 var curURL = document.URL;
 var newWindow = window.open(curURL, "_blank", "width=400, height=500");
-var delay = 1000;
+var heartBeat = 1000;
 
 function init() {
-    delay = 1000;
-    console.log('tick');
-    if(newWindow.document.readyState == 'complete'){
-        if (newWindow.$("#card-acquisition-page").hasClass("ui-page-active")) {
-            newWindow.history.back();
-        } else if (newWindow.$("#pvp-event-encounter-page").hasClass("ui-page-active")) {
-            newWindow.location.href = curURL;
-        } else if (newWindow.$("#parts-complete-page").hasClass("ui-page-active")) {
+    if(newWindow.document.readyState == 'complete' && newWindow.$('.ui-loading').length == 0){
+        if (newWindow.$("#card-acquisition-page").hasClass("ui-page-active") ||
+                newWindow.$("#pvp-event-encounter-page").hasClass("ui-page-active") ||
+                newWindow.$("#parts-complete-page").hasClass("ui-page-active") ||
+                newWindow.$("#level-up-page").hasClass("ui-page-active") ||
+                newWindow.$("#encounter-other-player-page").hasClass("ui-page-active") ||
+                newWindow.$("#parts-pvp-acquisition-page").hasClass("ui-page-active")) {
             newWindow.history.back();
         } else if (newWindow.$("#npc-battle-confirm-page").hasClass("ui-page-active")) {
             $link = newWindow.$('a:first');
             $link[0].click();
         } else if (newWindow.$("#won").hasClass("ui-page-active")) {
             $link = newWindow.$('a:first');
-            //$link[0].click();
+            $link[0].click();
         } else if (newWindow.$("#negotiation-failed-page").hasClass("ui-page-active")) {
             newWindow.location.href = curURL;
         } else if (newWindow.$("#negotiation-page").hasClass("ui-page-active")) {
             $link = newWindow.$('a:first');
             $link[0].click();
         } else if (newWindow.$("#drama-page").hasClass("ui-page-active")) {
-            newWindow.location.href = "http://zc2.ayakashi.zynga.com/app.php?hid=last&encounter_battle_mode=1&_c=npc_battle&action=battle_scene";
+            newWindow.location.href = unescape(newWindow.$('base').attr('href').split('&')[1]).split('next_url=')[1];
         } else if (newWindow.$("#won").hasClass("ui-page-active")) { //ISSUE
             $link = newWindow.$('a:first');
             $link[0].click();
-        } else if (newWindow.$("#negotiation-failed-page").hasClass("ui-page-active")) {
-            newWindow.location.href = curURL;
         } else if (newWindow.$("#negotiation-page").hasClass("ui-page-active")) {
             $link = newWindow.$('a:first');
             $link[0].click();
@@ -44,27 +41,14 @@ function init() {
             newWindow.location.href = str;
         } else if (newWindow.$("#battle-page").hasClass("ui-page-active")) {
             newWindow.$("#node-7").trigger("click");
-        } else if (newWindow.$("#treasure-acquisition-page").hasClass("ui-page-active")) {
-            newWindow.history.back();
-        } else if (newWindow.$("#level-up-page").hasClass("ui-page-active")) {
-            newWindow.history.back();
-        } else if (newWindow.$("#encounter-other-player-page").hasClass("ui-page-active")) {
-            newWindow.history.back();
-        } else if (newWindow.$("#parts-pvp-acquisition-page").hasClass("ui-page-active")) {
-            newWindow.history.back();
-        } else {
-            if (!(newWindow.$("#do-adventure").hasClass("loading"))) {
-                console.log('if loading');
-                if (current < max) {
-                    newWindow.$("#do-adventure").trigger("click");
-                } else {
-                    continueClick = false;
-                }
-            } else {
-            }
+        } else if (newWindow.$("#stage-page").hasClass("ui-page-active")) {
+            //console.log('SOME WEIRD STATE');
+            newWindow.$("#do-adventure").trigger("click");
         }
+        setTimeout(init, heartBeat);
+    }else{
+        setTimeout(init, heartBeat);
     }
-    setTimeout(init, delay);
 }
 
 function start(){
